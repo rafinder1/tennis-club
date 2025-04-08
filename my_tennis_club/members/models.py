df68263e9ca1ec31e9473f701383a8f9e78f8839
rafinder1 <rafpro33@gmail.com>
2025-04-08 18:58:55 +0200

    created model Member and makemigations

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,1 +1,17 @@
-# Create your models here.
+"""
+This module contains the definition of the Member model for the MyTennisClub application.
+"""
+
+from django.db import models
+
+
+class Member(models.Model):
+    """
+    Represents a member of the tennis club with basic information like
+    first name, last name, phone number, and the date they joined.
+    """
+
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
