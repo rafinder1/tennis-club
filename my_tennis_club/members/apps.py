c0d266eaaac86105c8f88f9c9616355468c58b19
rafinder1 <rafpro33@gmail.com>
2025-04-08 17:31:23 +0200

    created django app

---
+++ my_tennis_club/members/apps.py
@@ -0,0 +1,10 @@
+"""App configuration for the members app."""
+
+from django.apps import AppConfig
+
+
+class MembersConfig(AppConfig):
+    """Configuration class for the members app."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "members"
