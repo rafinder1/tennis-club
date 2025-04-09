4ca7375684577397a1234ef77a147e8c6aae911e
rafinder1 <rafpro33@gmail.com>
2025-04-09 18:24:01 +0200

    changed admin view - app members

--- my_tennis_club/members/admin.py
+++ my_tennis_club/members/admin.py
@@ -1,1 +1,18 @@
-# Register your models here.
+"""Admin configuration for the members app."""
+
+from django.contrib import admin
+
+from .models import Member
+
+
+class MemberAdmin(admin.ModelAdmin):
+    """Admin interface options for the Member model."""
+
+    list_display = (
+        "firstname",
+        "lastname",
+        "joined_date",
+    )
+
+
+admin.site.register(Member, MemberAdmin)
